5d13ce34a8205462fa6dd24b71880cfafd5d4926
jbrown-xentity <jbrown@xentity.com>
2020-11-30 14:40:33 -0700

    Build reqs and search reqs locally

--- vendor-requirements.sh
+++ vendor-requirements.sh
@@ -26,6 +26,6 @@ mkdir vendor
 chown vcap.vcap vendor
 mkdir src
 chown vcap.vcap src
-su - vcap -c 'cd app && pip download -r requirements.txt --no-binary=:none: -d vendor'
+su - vcap -c 'cd app && pip download -r requirements.txt --no-binary=:none: -d vendor --exists-action=w'
 
 EOF
