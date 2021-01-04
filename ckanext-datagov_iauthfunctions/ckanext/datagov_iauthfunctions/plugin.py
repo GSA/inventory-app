0cd77f6bd7f6cbedf023efb39901746c62aaace6
Preston Sharpe <psharpe@xentity.com>
2021-01-04 14:35:46 -0700

    update plugin.py & user register

--- ckanext-datagov_iauthfunctions/ckanext/datagov_iauthfunctions/plugin.py
+++ ckanext-datagov_iauthfunctions/ckanext/datagov_iauthfunctions/plugin.py
@@ -2,35 +2,49 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+default_message = 'You must be logged to perform this action.'
+
+def redirect_to_login():
+    return redirect("http://www.example.com", code=302)
+
+
+@plugins.toolkit.auth_allow_anonymous_access
+def group_list(context, data_dict=None):
+    user_name = context['user']
+
+    if user_name:
+        return {'success' : True}
+    else:
+        return {'success' : False, 'msg' : default_message}
+
+@plugins.toolkit.auth_allow_anonymous_access
+def site_read(context, data_dict=None):
+    user_name = context['user']
+
+    if user_name:
+        return {'success' : True}
+    else:
+        return {'success' : False, 'msg' : default_message}
+
+# resources should be the only 'action' that defaults to True
+@plugins.toolkit.auth_allow_anonymous_access
+def resource_download(context, data_dict=None):
+    return {'success' : True}
+
+@plugins.toolkit.auth_allow_anonymous_access
+def resource_read(context, data_dict=None):
+    return {'success' : True}
+
+@plugins.toolkit.auth_allow_anonymous_access
+def resource_view(context, data_dict=None):
+    return {'success' : True}
+
 class Datagov_IauthfunctionsPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IAuthFunctions)
 
-    # IConfigurer
-
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'datagov_iauthfunctions')
-
-    def read(self, entity):
-        '''
-        IPackageController.read && IOrganizationController.read
-        page must not be accessible by visitors
-        '''
-        visitor_allowed_actions = [
-            'resource_download',  # download resource file
-            'resource_read',  # resource read page
-            'resource_view'  # resource view (data explorer)
-        ]
-        if not c.user and c.action not in visitor_allowed_actions:
-            abort(401, _('Not authorized to see this page'))
-            
-    def before_search(self, search_params):
-        '''
-        IPackageController.search
-        page must not be accessible by visitors
-        '''
-        if not c.user:
-            abort(401, _('Not authorized to see this page'))
-
-        return search_params
+    def get_auth_functions(self):
+        return {'group_list' : group_list,
+                'site_read' : site_read,
+                'resource_download' : resource_download,
+                'resource_read' : resource_read,
+                'resource_view' : resource_view}
