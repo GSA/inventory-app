cef03cd9a3d76f5d31d4adefeef5bcb49594be40
Chris MacDermaid <christopher.macdermaid@gsa.gov>
2021-02-01 14:02:14 -0700

    Update unit test #2

--- ckanext-datagov_inventory/ckanext/datagov_inventory/tests/logic/auth/test_auth.py
+++ ckanext-datagov_inventory/ckanext/datagov_inventory/tests/logic/auth/test_auth.py
@@ -1,12 +1,13 @@
 """Tests for datagov_inventory plugin.py."""
 
 from nose.tools import assert_raises
-from nose.tools import assert_equal
 
 import ckan.model as model
 import ckan.tests.factories as factories
 import ckan.tests.helpers as helpers
 import ckan.logic as logic
+
+import ckanext.datastore.tests.helpers as test_helpers
 
 import logging
 
@@ -26,7 +27,9 @@ class TestDatagovInventoryAuth(object):
         '''Nose runs this method before each test method in our test class.'''
 
         # Start with a clean database for each test
+        self._clean_datastore
         model.repo.rebuild_db()
+
 
     def teardown(self):
         '''Nose runs this method after each test method in our test class.'''
@@ -55,12 +58,21 @@ class TestDatagovInventoryAuth(object):
         org_dict = factories.Organization(users=org_users,
                                           name='doi')
 
+    def _clean_datastore(self):
+        engine = test_helpers.db.get_write_engine()
+        test_helpers.rebuild_all_dbs(
+            test_helpers.orm.scoped_session(
+                test_helpers.orm.sessionmaker(bind=engine)
+            )
+        )
+
     def _setup_private_gsa_dataset(self):
 
         private_dataset_params = {
             'private': 'true',
             'name': 'private_test_package',
             'title': 'private test package',
+            'id': 'private_package_id',
             'tag_string': 'test_package',
             'modified': '2014-04-04',
             'publisher': 'GSA',
@@ -81,13 +93,10 @@ class TestDatagovInventoryAuth(object):
                     'description': 'description'}
             ]
         }
-        factories.Dataset(**private_dataset_params)
-        return({
-            'model': model,
-            'ignore_auth': False,
-            'user': '',
-            'id' : 'private_resource_id'
-        })
+        dataset = factories.Dataset(**private_dataset_params)
+        log.debug('Private Dataset Id %s', dataset['id'])
+        # Return id string for the package and resoruce just created
+        return({'package_id' : 'private_package_id', 'resource_id': 'private_resource_id'})
 
     def _setup_public_gsa_dataset(self):
 
@@ -95,6 +104,7 @@ class TestDatagovInventoryAuth(object):
             'private': 'true',
             'name': 'public_test_package',
             'title': 'public test package',
+            'id' : 'public_package_id',
             'tag_string': 'test_package',
             'modified': '2014-04-04',
             'publisher': 'GSA',
@@ -115,18 +125,12 @@ class TestDatagovInventoryAuth(object):
                     'description': 'description'}
             ]
         }
-
-        # Create public package/dataset
         factories.Dataset(**public_dataset_params)
-        return({
-            'model': model,
-            'ignore_auth': False,
-            'user': '',
-            'id' : 'public_resource_id'
-        })
+        # Return id string for the package and resoruce just created
+        return({'package_id' : 'public_package_id', 'resource_id': 'public_resource_id'})
 
 
-    def assert_user_authorization(self, auth_function, context, expected_user_access_dict):
+    def assert_user_authorization(self, auth_function, object_id, expected_user_access_dict):
         # Assert the expected_user_access_dict is complete for our matrix of access roles.
         #  It's an error if the test case is missing an expectation.
         assert 'gsa_admin' in expected_user_access_dict
@@ -136,41 +140,46 @@ class TestDatagovInventoryAuth(object):
         assert 'anonymous' in expected_user_access_dict
 
         for user in expected_user_access_dict:
-            context['user'] = user  # apply the user to the given context
-            actual_authorization = helpers.call_auth(auth_function, context=context) # assert NotAuthorized and return a boolean value for assertion
-            assert actual_authorization == expected_user_access_dict[user]
+            context = {
+                'model': model,
+                'ignore_auth': False,
+                'user': user}
+            log.debug('For Loop Context: %s', context)
+            if expected_user_access_dict[user]:
+                actual_authorization = helpers.call_auth(auth_function, context=context, id=object_id)
+                assert actual_authorization == expected_user_access_dict[user]
+            else:
+                assert_raises(logic.NotAuthorized, helpers.call_auth, auth_function, context=context, id=object_id)
 
     def test_auth_resource_show_for_private_gsa_dataset(self):
         is_allowed = True
         is_denied = False
         
+        # Create test users and test data
         self._setup_test_orgs_users()
-        context = self._setup_private_gsa_dataset()
+        dataset = self._setup_private_gsa_dataset()
 
-        self.assert_user_authorization('resource_show', context, {
+        self.assert_user_authorization('resource_show', dataset['resource_id'], {
             'gsa_admin': is_allowed,
             'gsa_editor': is_allowed,
             'gsa_member': is_allowed,
             'doi_member': is_allowed,
             'anonymous': is_allowed
-        }
+        })
 
     def test_auth_package_show_for_private_gsa_dataset(self):
         is_allowed = True
         is_denied = False
 
+        # Create test users and test data
         self._setup_test_orgs_users()
-        context = self._setup_private_gsa_dataset()
-
-        self.assert_user_authorization('resource_show', context, {
+        dataset = self._setup_private_gsa_dataset()
+    
+        log.debug('Test package_show Package Id %s', dataset)
+        self.assert_user_authorization('package_show', dataset['package_id'], {
             'gsa_admin': is_allowed,
             'gsa_editor': is_allowed,
             'gsa_member': is_allowed,
-            'doi_member': is_allowed,
-            'anonymous': is_allowed
-        }
-
-
-)
-
-    
+            'doi_member': is_denied,
+            'anonymous': is_denied
+        })
