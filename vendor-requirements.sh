4faf46e334149caa411a6d5c1be621a3cf47ebad
Aaron D Borden <adborden@a14n.net>
2021-02-09 17:26:23 -0800

    fixup

--- vendor-requirements.sh
+++ vendor-requirements.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o pipefail
 
 # Install any packaged dependencies for our vendored packages
-apt-get -y update
-apt-get -y install swig build-essential python-dev libssl-dev
+sudo apt-get -y update
+sudo apt-get -y install swig build-essential python-dev libssl-dev
 
 pip wheel -w vendor -r requirements.txt
