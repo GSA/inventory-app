49ee7e9349f816ae1c33dfc911af851414ce869d
Aaron D Borden <adborden@a14n.net>
2021-02-09 17:52:52 -0800

    fixup

--- vendor-requirements.sh
+++ vendor-requirements.sh
@@ -8,4 +8,4 @@ sudo apt-get -y update
 sudo apt-get -y install swig build-essential python-dev libssl-dev
 
 pip install --user wheel
-pip wheel -w vendor -r requirements.txt
+pip wheel -w vendor --no-binary :none: -r requirements.txt
