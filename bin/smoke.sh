78e6e30f883d647a56583959df6a4a4c4f7dddca
jbrown-xentity <jbrown@xentity.com>
2021-02-12 07:53:58 -0700

    fix smoke to ignore full curl output

--- bin/smoke.sh
+++ bin/smoke.sh
@@ -8,6 +8,6 @@ set -x
 # TODO move smoke tests to python
 
 curl --fail --silent ${APP_URL}/api/action/status_show?$(date +%s) > /dev/null
-[ "200" == "$(curl --silent --fail --write-out %{http_code} ${APP_URL}/dataset?$(date +%s))" ]
+[ "200" == "$(curl --silent --fail -o /dev/null --write-out %{http_code} ${APP_URL}/dataset?$(date +%s))" ]
 
 echo ok
