6d544175c76c67c855d770b4b688c2c3a60bc5ee
Aaron D Borden <adborden@a14n.net>
2021-02-17 17:08:20 -0800

    Helper function for cfstart.sh
    Use a function to get VCAP service credentials. Pull saml2 key from VCAP
    service.

--- cfstart.sh
+++ cfstart.sh
@@ -11,6 +11,13 @@ function ckan () {
     paster --plugin=ckan "$@"
 }
 
+function vcap_get_service () {
+  local path service_name
+  service_name="$1"
+  path="$2"
+  echo $VCAP_SERVICES | jq --raw-output --arg service_name "$service_name" ".[][] | select(.name == \$service_name) | $path"
+}
+
 # We need to know the application name ...
 APP_NAME=$(echo $VCAP_APPLICATION | jq -r '.application_name')
 ORG_NAME=$(echo $VCAP_APPLICATION | jq -r '.organization_name')
@@ -28,44 +35,47 @@ SVC_REDIS="${APP_NAME}-redis"
 SVC_SECRETS="${APP_NAME}-secrets"
 
 # We need specific datastore URL components so we can construct another URL for the read-only user
-DS_HOST=$(echo $VCAP_SERVICES | jq -r --arg SVC_DATASTORE $SVC_DATASTORE '.[][] | select(.name == $SVC_DATASTORE) | .credentials.host')
-DS_PORT=$(echo $VCAP_SERVICES | jq -r --arg SVC_DATASTORE $SVC_DATASTORE '.[][] | select(.name == $SVC_DATASTORE) | .credentials.port')
-DS_DBNAME=$(echo $VCAP_SERVICES | jq -r --arg SVC_DATASTORE $SVC_DATASTORE '.[][] | select(.name == $SVC_DATASTORE) | .credentials.db_name')
+DS_HOST=$(vcap_get_service $SVC_DATASTORE .credentials.host)
+DS_PORT=$(vcap_get_service $SVC_DATASTORE .credentials.port)
+DS_DBNAME=$(vcap_get_service $SVC_DATASTORE .credentials.db_name)
 
-# We need the redis credentials for ckan to access redis, and we need to build the url to use the rediss 
-REDIS_HOST=$(echo $VCAP_SERVICES | jq -r --arg SVC_REDIS $SVC_REDIS '.[][] | select(.name == $SVC_REDIS) | .credentials.host')
-REDIS_PASSWORD=$(echo $VCAP_SERVICES | jq -r --arg SVC_REDIS $SVC_REDIS '.[][] | select(.name == $SVC_REDIS) | .credentials.password')
-REDIS_PORT=$(echo $VCAP_SERVICES | jq -r --arg SVC_REDIS $SVC_REDIS '.[][] | select(.name == $SVC_REDIS) | .credentials.port')
+# We need the redis credentials for ckan to access redis, and we need to build the url to use the rediss
+REDIS_HOST=$(vcap_get_service $SVC_REDIS .credentials.host)
+REDIS_PASSWORD=$(vcap_get_service $SVC_REDIS .credentials.password)
+REDIS_PORT=$(vcap_get_service $SVC_REDIS .credentials.port)
 
+SAML2_PRIVATE_KEY=$(vcap_get_service $SVC_SECRETS .credentials.SAML2_PRIVATE_KEY)
 SECRETS_DIR=$(mktemp -d)
+
 export CKANEXT__SAML2AUTH__KEY_FILE_PATH=${SECRETS_DIR}/saml2_key.pem
 export CKANEXT__SAML2AUTH__CERT_FILE_PATH=${SECRETS_DIR}/saml2_certificate.pem
 
-echo "$SAML2_PRIVATE_KEY" | base64 -d > $CKANEXT__SAML2AUTH__KEY_FILE_PATH
-echo "$SAML2_CERTIFICATE" > $CKANEXT__SAML2AUTH__CERT_FILE_PATH
-
 # We need the secret credentials for various application components (DB configuration, license keys, etc)
-DS_RO_PASSWORD=$(echo $VCAP_SERVICES | jq -r --arg SVC_SECRETS $SVC_SECRETS '.[][] | select(.name == $SVC_SECRETS) | .credentials.DS_RO_PASSWORD')
-export NEW_RELIC_LICENSE_KEY=$(echo $VCAP_SERVICES | jq -r --arg SVC_SECRETS $SVC_SECRETS '.[][] | select(.name == $SVC_SECRETS) | .credentials.NEW_RELIC_LICENSE_KEY')
-export CKAN___BEAKER__SESSION__SECRET=$(echo $VCAP_SERVICES | jq -r --arg SVC_SECRETS $SVC_SECRETS '.[][] | select(.name == $SVC_SECRETS) | .credentials.CKAN___BEAKER__SESSION__SECRET')
+DS_RO_PASSWORD=$(vcap_get_service $SVC_SECRETS .credentials.DS_RO_PASSWORD)
+export NEW_RELIC_LICENSE_KEY=$(vcap_get_service $SVC_SECRETS .credentials.NEW_RELIC_LICENSE_KEY)
+export CKAN___BEAKER__SESSION__SECRET=$(vcap_get_service $SVC_SECRETS .credentials.CKAN___BEAKER__SESSION__SECRET)
 
 # ckan reads some environment variables... https://docs.ckan.org/en/2.8/maintaining/configuration.html#environment-variables
-export CKAN_SQLALCHEMY_URL=$(echo $VCAP_SERVICES | jq -r --arg SVC_DATABASE $SVC_DATABASE '.[][] | select(.name == $SVC_DATABASE) | .credentials.uri')
+export CKAN_SQLALCHEMY_URL=$(vcap_get_service $SVC_DATABASE .credentials.uri)
 export CKAN___BEAKER__SESSION__URL=${CKAN_SQLALCHEMY_URL}
 export CKAN_SITE_URL=https://$APP_URL
 export CKAN_SITE_ID=$ORG_NAME-$SPACE_NAME-$APP_NAME
 export CKAN_SOLR_URL=$SOLR_URL
-export CKAN_DATASTORE_WRITE_URL=$(echo $VCAP_SERVICES | jq -r --arg SVC_DATASTORE $SVC_DATASTORE '.[][] | select(.name == $SVC_DATASTORE) | .credentials.uri')
+export CKAN_DATASTORE_WRITE_URL=$(vcap_get_service $SVC_DATASTORE .credentials.uri)
 export CKAN_DATASTORE_READ_URL=postgres://$DS_RO_USER:$DS_RO_PASSWORD@$DS_HOST:$DS_PORT/$DS_DBNAME
 export CKAN_REDIS_URL=rediss://:$REDIS_PASSWORD@$REDIS_HOST:$REDIS_PORT
 export CKAN_STORAGE_PATH=/home/vcap/app/files
 
 # Use ckanext-envvars to import other configurations...
-export CKANEXT__S3FILESTORE__REGION_NAME=$(echo $VCAP_SERVICES | jq -r --arg SVC_S3 $SVC_S3 '.[][] | select(.name == $SVC_S3) | .credentials.region')
+export CKANEXT__S3FILESTORE__REGION_NAME=$(vcap_get_service $SVC_S3 .credentials.region)
 export CKANEXT__S3FILESTORE__HOST_NAME=https://s3-$CKANEXT__S3FILESTORE__REGION_NAME.amazonaws.com
-export CKANEXT__S3FILESTORE__AWS_ACCESS_KEY_ID=$(echo $VCAP_SERVICES | jq -r --arg SVC_S3 $SVC_S3 '.[][] | select(.name == $SVC_S3) | .credentials.access_key_id')
-export CKANEXT__S3FILESTORE__AWS_SECRET_ACCESS_KEY=$(echo $VCAP_SERVICES | jq -r --arg SVC_S3 $SVC_S3 '.[][] | select(.name == $SVC_S3) | .credentials.secret_access_key')
-export CKANEXT__S3FILESTORE__AWS_BUCKET_NAME=$(echo $VCAP_SERVICES | jq -r --arg SVC_S3 $SVC_S3 '.[][] | select(.name == $SVC_S3) | .credentials.bucket')
+export CKANEXT__S3FILESTORE__AWS_ACCESS_KEY_ID=$(vcap_get_service $SVC_S3 .credentials.access_key_id)
+export CKANEXT__S3FILESTORE__AWS_SECRET_ACCESS_KEY=$(vcap_get_service $SVC_S3 .credentials.secret_access_key)
+export CKANEXT__S3FILESTORE__AWS_BUCKET_NAME=$(vcap_get_service $SVC_S3 .credentials.bucket)
+
+# Write out any files
+echo "$SAML2_PRIVATE_KEY" | base64 -d > $CKANEXT__SAML2AUTH__KEY_FILE_PATH
+echo "$SAML2_CERTIFICATE" > $CKANEXT__SAML2AUTH__CERT_FILE_PATH
 
 # Edit the config file to use validate debug is off and utilizes the correct port
 export CKAN_INI=config/production.ini
