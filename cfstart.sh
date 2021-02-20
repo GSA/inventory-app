dc9e7cdaedc97a19b2d30874b9a2c722ad211d06
Aaron D Borden <adborden@a14n.net>
2021-02-19 16:22:53 -0800

    Clean up configuration
    The main motivation for this is to prevent us accidentally using an insecure
    default because a configuration item was mapped incorrectly. For example, it
    would be a disaster if the beaker.session.secret was set to an insecure default
    because we typo'd the environment variable name.
    
    - Make sure every CKAN configuration item exists in the production.ini file (as
    a comment).
    - Avoid setting a default, a missing configuration item should fail the build.
    - Include the env variable name expected so it's easy to grep for the setting.

--- cfstart.sh
+++ cfstart.sh
@@ -12,9 +12,10 @@ function ckan () {
 }
 
 function vcap_get_service () {
-  local path service_name
-  service_name="$1"
+  local path name
+  name="$1"
   path="$2"
+  service_name=${APP_NAME}-${name}
   echo $VCAP_SERVICES | jq --raw-output --arg service_name "$service_name" ".[][] | select(.name == \$service_name) | $path"
 }
 
@@ -24,59 +25,43 @@ SHARED_DIR=$(mktemp -d)
 
 # We need to know the application name ...
 APP_NAME=$(echo $VCAP_APPLICATION | jq -r '.application_name')
-ORG_NAME=$(echo $VCAP_APPLICATION | jq -r '.organization_name')
-SPACE_NAME=$(echo $VCAP_APPLICATION | jq -r '.space_name')
-
-# We need the public URL for the configuration file
 APP_URL=$(echo $VCAP_APPLICATION | jq -r '.application_uris[0]')
 
-# ... from which we can guess the service names
-
-SVC_DATABASE="${APP_NAME}-db"
-SVC_DATASTORE="${APP_NAME}-datastore"
-SVC_S3="${APP_NAME}-s3"
-SVC_REDIS="${APP_NAME}-redis"
-SVC_SECRETS="${APP_NAME}-secrets"
-
 # We need specific datastore URL components so we can construct another URL for the read-only user
-DS_HOST=$(vcap_get_service $SVC_DATASTORE .credentials.host)
-DS_PORT=$(vcap_get_service $SVC_DATASTORE .credentials.port)
-DS_DBNAME=$(vcap_get_service $SVC_DATASTORE .credentials.db_name)
+DS_HOST=$(vcap_get_service datastore .credentials.host)
+DS_PORT=$(vcap_get_service datastore .credentials.port)
+DS_DBNAME=$(vcap_get_service datastore .credentials.db_name)
 
 # We need the redis credentials for ckan to access redis, and we need to build the url to use the rediss
-REDIS_HOST=$(vcap_get_service $SVC_REDIS .credentials.host)
-REDIS_PASSWORD=$(vcap_get_service $SVC_REDIS .credentials.password)
-REDIS_PORT=$(vcap_get_service $SVC_REDIS .credentials.port)
+REDIS_HOST=$(vcap_get_service redis .credentials.host)
+REDIS_PASSWORD=$(vcap_get_service redis .credentials.password)
+REDIS_PORT=$(vcap_get_service redis .credentials.port)
 
-SAML2_PRIVATE_KEY=$(vcap_get_service $SVC_SECRETS .credentials.SAML2_PRIVATE_KEY)
+SAML2_PRIVATE_KEY=$(vcap_get_service secrets .credentials.SAML2_PRIVATE_KEY)
 
 export CKANEXT__SAML2AUTH__KEY_FILE_PATH=${CONFIG_DIR}/saml2_key.pem
 export CKANEXT__SAML2AUTH__CERT_FILE_PATH=${CONFIG_DIR}/saml2_certificate.pem
-export CKANEXT__SAML2AUTH__IDP_METADATA__REMOTE_CERT=${CONFIG_DIR}/saml2_idp_certificate.pem
-#export CKANEXT__SAML2AUTH__IDP_METADATA__LOCAL_PATH=${CONFIG_DIR}/idp.xml
 
 # We need the secret credentials for various application components (DB configuration, license keys, etc)
-DS_RO_PASSWORD=$(vcap_get_service $SVC_SECRETS .credentials.DS_RO_PASSWORD)
-export NEW_RELIC_LICENSE_KEY=$(vcap_get_service $SVC_SECRETS .credentials.NEW_RELIC_LICENSE_KEY)
-export CKAN___BEAKER__SESSION__SECRET=$(vcap_get_service $SVC_SECRETS .credentials.CKAN___BEAKER__SESSION__SECRET)
+DS_RO_PASSWORD=$(vcap_get_service secrets .credentials.DS_RO_PASSWORD)
+export NEW_RELIC_LICENSE_KEY=$(vcap_get_service secrets .credentials.NEW_RELIC_LICENSE_KEY)
+export CKAN___BEAKER__SESSION__SECRET=$(vcap_get_service secrets .credentials.CKAN___BEAKER__SESSION__SECRET)
 
 # ckan reads some environment variables... https://docs.ckan.org/en/2.8/maintaining/configuration.html#environment-variables
-export CKAN_SQLALCHEMY_URL=$(vcap_get_service $SVC_DATABASE .credentials.uri)
+export CKAN_SQLALCHEMY_URL=$(vcap_get_service db .credentials.uri)
 export CKAN___BEAKER__SESSION__URL=${CKAN_SQLALCHEMY_URL}
 export CKAN_SITE_URL=https://$APP_URL
-export CKAN_SITE_ID=$ORG_NAME-$SPACE_NAME-$APP_NAME
-export CKAN_SOLR_URL=$SOLR_URL
-export CKAN_DATASTORE_WRITE_URL=$(vcap_get_service $SVC_DATASTORE .credentials.uri)
+export CKAN_DATASTORE_WRITE_URL=$(vcap_get_service datastore .credentials.uri)
 export CKAN_DATASTORE_READ_URL=postgres://$DS_RO_USER:$DS_RO_PASSWORD@$DS_HOST:$DS_PORT/$DS_DBNAME
 export CKAN_REDIS_URL=rediss://:$REDIS_PASSWORD@$REDIS_HOST:$REDIS_PORT
 export CKAN_STORAGE_PATH=${SHARED_DIR}/files
 
 # Use ckanext-envvars to import other configurations...
-export CKANEXT__S3FILESTORE__REGION_NAME=$(vcap_get_service $SVC_S3 .credentials.region)
+export CKANEXT__S3FILESTORE__REGION_NAME=$(vcap_get_service s3 .credentials.region)
 export CKANEXT__S3FILESTORE__HOST_NAME=https://s3-$CKANEXT__S3FILESTORE__REGION_NAME.amazonaws.com
-export CKANEXT__S3FILESTORE__AWS_ACCESS_KEY_ID=$(vcap_get_service $SVC_S3 .credentials.access_key_id)
-export CKANEXT__S3FILESTORE__AWS_SECRET_ACCESS_KEY=$(vcap_get_service $SVC_S3 .credentials.secret_access_key)
-export CKANEXT__S3FILESTORE__AWS_BUCKET_NAME=$(vcap_get_service $SVC_S3 .credentials.bucket)
+export CKANEXT__S3FILESTORE__AWS_ACCESS_KEY_ID=$(vcap_get_service s3 .credentials.access_key_id)
+export CKANEXT__S3FILESTORE__AWS_SECRET_ACCESS_KEY=$(vcap_get_service s3 .credentials.secret_access_key)
+export CKANEXT__S3FILESTORE__AWS_BUCKET_NAME=$(vcap_get_service s3 .credentials.bucket)
 
 # Write out any files and directories
 mkdir -p $CKAN_STORAGE_PATH
