82e7618486ba92830caddafb489a299a9bd8d106
jbrown-xentity <jbrown@xentity.com>
2021-08-09 03:35:36 -0600

    Add ckan_resource.csv and update vendor pip

--- vendor-requirements.sh
+++ vendor-requirements.sh
@@ -21,7 +21,7 @@ apt-get -y update
 apt-get -y install swig build-essential python-dev libssl-dev
 
 # Install PIP
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o /tmp/get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 python /tmp/get-pip.py
 
 # As the VCAP user, cache .whls based on the frozen requirements for vendoring
