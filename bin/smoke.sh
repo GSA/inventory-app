9a9962df6ea4d3eb2cbbc92f6af97a22be9a5608
jbrown-xentity <jbrown@xentity.com>
2021-08-24 09:55:15 -0600

    Add wait to smoke test

--- bin/smoke.sh
+++ bin/smoke.sh
@@ -7,6 +7,9 @@ set -x
 
 # TODO move smoke tests to python
 
+# Application may not be fully available immediately, wait 5 seconds
+sleep 5
+
 curl --fail --silent ${APP_URL}/api/action/status_show?$(date +%s) > /dev/null
 [ "403" == "$(curl --silent --output /dev/null --write-out %{http_code} ${APP_URL}/dataset?$(date +%s))" ]
 
