0957327b00ad670da29c14e54688b71d14c9cad0
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-12-07 13:07:56 -0800

    Automate creation of CF services if they don't exist

---
+++ create-cloudgov-services.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e 
+
+# If an argument was provided, use it as the service name prefix. 
+# Otherwise default to "inventory".
+app_name=${1:-inventory}
+
+# Get the current space and trim leading whitespace
+space=$(cf target | grep space | cut -d : -f 2 | xargs)
+
+# Production and staging should use bigger DB and Redis instances
+if [ "$space" = "prod" ] || [ "$space" = "staging" ]; then
+    # TODO: Figure out what RDS plan we should be using for datastore in production
+    cf service "${app_name}-datastore" > /dev/null 2>&1 || cf create-service aws-rds micro-psql "${app_name}-datastore"             --wait&
+    cf service "${app_name}-db"        > /dev/null 2>&1 || cf create-service aws-rds large-gp-psql "${app_name}-db" -c '{"version": "11"}' --wait&
+    cf service "${app_name}-redis"     > /dev/null 2>&1 || cf create-service aws-elasticache-redis redis-3node "${app_name}-redis"  --wait&
+else
+    cf service "${app_name}-datastore" > /dev/null 2>&1 || cf create-service aws-rds micro-psql "${app_name}-datastore"             --wait&
+    cf service "${app_name}-db"        > /dev/null 2>&1 || cf create-service aws-rds small-psql "${app_name}-db"                    --wait&
+    cf service "${app_name}-redis"     > /dev/null 2>&1 || cf create-service aws-elasticache-redis redis-dev "${app_name}-redis"    --wait&
+fi
+cf service "${app_name}-solr"          > /dev/null 2>&1 || cf create-service solr-cloud base "${app_name}-solr" -c solr/service-config.json -b "ssb-solr-gsa-datagov-${space}" --wait&
+cf service "${app_name}-s3"            > /dev/null 2>&1 || cf create-service s3 basic-sandbox "${app_name}-s3"                       --wait&
+
+# Wait until all three services are ready
+wait
