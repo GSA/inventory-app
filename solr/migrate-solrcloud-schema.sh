c3718cb2e498a5db82db2a7fe8eaa529a4f50776
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-03-12 14:29:26 -0500

    fix: pull in Solr 8 fix from catalog implementation
    Catalog and Inventory are tracking solr startup differently, but I guess that's fine for now

--- solr/migrate-solrcloud-schema.sh
+++ solr/migrate-solrcloud-schema.sh
@@ -24,16 +24,26 @@ if ! (curl --get --fail --location-trusted  --user $CKAN_SOLR_USER:$CKAN_SOLR_PA
     --data-urlencode wt=json | grep -q $COLLECTION_NAME); then
 
     # Zip solr configSet
-    cd solr && zip ckan_2.9_solr_config.zip \
-        currency.xml  elevate.xml  protwords.txt  schema.xml  solrconfig.xml stopwords.txt  synonyms.txt
+    cd solr
+
+    # Get managed-schema
+    CKAN_BRANCH="dev-v2.9"
+    curl https://raw.githubusercontent.com/ckan/ckan/$CKAN_BRANCH/ckan/config/solr/schema.xml -o schema.xml
+
+    # Fix from https://github.com/ckan/ckan/issues/5585#issuecomment-953586246
+    sed -i "s/<defaultSearchField>text<\/defaultSearchField>/<df>text<\/df>/" schema.xml
+    sed -i "s/<solrQueryParser defaultOperator=\"AND\"\/>/<solrQueryParser q.op=\"AND\"\/>/" schema.xml
+
+    zip ckan_2.9_solr_config.zip \
+        schema.xml solrconfig.xml protwords.txt stopwords.txt  synonyms.txt elevate.xml currency.xml
 
     echo "Uploading config set..."
-    curl --fail  --location-trusted --user $CKAN_SOLR_USER:$CKAN_SOLR_PASSWORD \
+    curl --fail --location-trusted --user $CKAN_SOLR_USER:$CKAN_SOLR_PASSWORD \
         "$CKAN_SOLR_BASE_URL/solr/admin/configs?action=upload&name=$COLLECTION_NAME" \
         --data-binary @ckan_2.9_solr_config.zip --header 'Content-Type:application/octet-stream'
 
     echo "Creating solr collection..."
-    curl --fail  --location-trusted --user $CKAN_SOLR_USER:$CKAN_SOLR_PASSWORD \
+    curl --fail --location-trusted --user $CKAN_SOLR_USER:$CKAN_SOLR_PASSWORD \
         "$CKAN_SOLR_BASE_URL/solr/admin/collections?action=create&name=$COLLECTION_NAME&collection.configName=$COLLECTION_NAME&numShards=1" \
         -X POST
     cd -
