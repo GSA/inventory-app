88169739621b749025eac412f7f097da76ff3c8d
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-06-27 13:39:01 -0400

    new: replicate solr setup scripts from catalog
    Provision solr-on-ecs instead of solrcloud and pass it the same parameters in the way it expects to receive them.  Setup local solr with the same script as solr-on-ecs (authentication, ckan core and all)

---
+++ solr/solr_setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+mkdir -p /tmp/ckan_config
+
+# Remove any restored EFS backups
+rm -rf /var/solr/data/aws-backup-restore*
+# TODO: replace existing (bad) core with the correct (good) restore directory
+
+# Check if users already exist
+SECURITY_FILE=/var/solr/data/security.json
+if [ -f "$SECURITY_FILE" ]; then
+  echo "Solr ckan and authentication are set up already :)"
+  exit 0;
+fi
+
+# add solr authentication
+cat <<SOLRAUTH > $SECURITY_FILE
+{
+"authentication":{
+   "blockUnknown": true,
+   "class":"solr.BasicAuthPlugin",
+   "credentials":{"catalog":"rJzrn+HooKn79Q+cfysdGKmMhJbtj0Q1bTokFud6f9o= eKuBUjAoBIkJAMYZxJU6HOKSchTAce+DoQrY5Vewu7I="},
+   "realm":"data.gov users",
+   "forwardCredentials": false
+},
+"authorization":{
+   "class":"solr.RuleBasedAuthorizationPlugin",
+   "permissions":[{"name":"security-edit",
+      "role":"admin"}],
+   "user-role":{"catalog":"admin"}
+}}
+SOLRAUTH
+
+# add solr config files for ckan 2.9
+wget -O /tmp/ckan_config/schema.xml https://raw.githubusercontent.com/GSA/inventory-app/main/solr/schema.xml
+wget -O /tmp/ckan_config/protwords.txt https://raw.githubusercontent.com/GSA/inventory-app/main/solr/protwords.txt
+wget -O /tmp/ckan_config/solrconfig.xml https://raw.githubusercontent.com/GSA/inventory-app/main/solr/solrconfig.xml
+wget -O /tmp/ckan_config/stopwords.txt https://raw.githubusercontent.com/GSA/inventory-app/main/solr/stopwords.txt
+wget -O /tmp/ckan_config/synonyms.txt https://raw.githubusercontent.com/GSA/inventory-app/main/solr/synonyms.txt
+
+#  group user solr:solr is 8983:8983 in solr docker image
+chown -R 8983:8983 /var/solr/data/
