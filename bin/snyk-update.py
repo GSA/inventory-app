b4a9eaf59a4b8e8349987630a349fc058407d44b
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-12-08 15:41:35 -0500

    new: copy from catalog
    https://github.com/GSA/catalog.data.gov/pull/677

---
+++ bin/snyk-update.py
@@ -0,0 +1,24 @@
+
+import json
+import os
+
+# Load results
+scan_file = open('scan.json')
+scan = json.load(scan_file)
+scan_file.close()
+
+# Make changes to fixable results
+remediations = scan['remediation']['pin']
+for k, v in remediations.items():
+    package, old_version = k.split('@')
+    new_version = v['upgradeTo'].split('@')[1]
+    print(package, old_version, new_version)
+
+    # Remove old version
+    os.system('sed -i "/%s/d" requirements.in.txt' % (package + "==" + old_version))
+    os.system('sed -i "/%s/d" requirements.in.txt' % (package + ">=" + old_version))
+
+    # Add new version
+    os.system("echo '%s' >> requirements.in.txt" % (package + ">=" + new_version))
+
+# TODO: Handle unfixable results
