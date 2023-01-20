00c28eabfdcc0bcfc0ea198c7ed8c1ccf0ba3866
Tyler Burton <tyler.burton@gsa.gov>
2023-01-20 11:10:16 -0600

    apply same fixes from catalog to inventory for local solr startup

--- solr/local_setup.sh
+++ solr/local_setup.sh
@@ -6,4 +6,7 @@ set -e
 /app/solr/solr_setup.sh
 
 # Start solr
-su -c "init-var-solr; precreate-core ckan /tmp/ckan_config; chown -R 8983:8983 /var/solr/data; solr-fg -Dsolr.lock.type=simple" -m solr
+su -c "\
+    export PATH=$PATH:/opt/docker-solr/scripts/:/opt/solr/bin/;\
+    init-var-solr; precreate-core ckan /tmp/ckan_config; chown -R 8983:8983 /var/solr/data; solr-fg -Dsolr.lock.type=simple \
+" -m solr
