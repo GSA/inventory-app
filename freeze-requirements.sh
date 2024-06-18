00550b11a0240da7229a4bb6915c73551dfcb210
Tyler Burton <tyler.burton@gsa.gov>
2024-06-18 16:47:56 -0500

    adds freeze requirements script; alphabetizes make commands

---
+++ freeze-requirements.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+venv=$(mktemp -d)
+
+function cleanup () {
+  rm -rf $venv
+}
+
+trap cleanup EXIT
+
+pip3 install virtualenv
+
+virtualenv $venv
+${venv}/bin/pip3 install setuptools==67.1.0
+${venv}/bin/pip3 install -r /app/requirements.in.txt
+
+${venv}/bin/pip3 freeze --all > /app/requirements.txt
