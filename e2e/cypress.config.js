b9aea6a64cb46a0f3239587659239d5ec4222b09
Fuhu Xia <fxia@reisystems.com>
2025-01-27 10:31:51 -0500

    upgrade cypress

---
+++ e2e/cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'cypress/results/output.xml',
+  },
+  videoCompression: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/integration/*.cy.js',
+    baseUrl: 'http://ckan:5000',
+  },
+})
