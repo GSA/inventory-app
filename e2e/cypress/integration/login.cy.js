217285e202728dc6cfc987f940d0fe1f9fa1f46d
Andrew Kuny <akuny@flexion.us>
2026-07-08 15:37:09 -0400

    6015: Add cookie inspection to Cypress test

--- e2e/cypress/integration/login.cy.js
+++ e2e/cypress/integration/login.cy.js
@@ -1,13 +1,23 @@
-describe('Login', () => {
-    
-    it('Invalid user login attempt', () => {
-        cy.login('not-user', 'not-password', true)
-        cy.contains('Login failed.')
+describe("Login", () => {
+  it("Invalid user login attempt", () => {
+    cy.login("not-user", "not-password", true);
+    cy.contains("Login failed.");
+  });
+
+  it("Valid login attempt", () => {
+    cy.login();
+    cy.get(".nav-tabs>li>a").should("contain", "My Organizations");
+  });
+
+  it("Auth cookie has secure attributes", () => {
+    cy.login();
+    cy.getCookie("ckan").should((cookie) => {
+      expect(cookie).to.exist;
+      expect(cookie.httpOnly).to.be.true;
+      // Secure flag only applies over HTTPS; in CI/local HTTP environments it will be false
+      // In production with HTTPS and nginx proxy, it will be true
+      // expect(cookie.secure).to.be.true;
+      expect(cookie.sameSite).to.eq("lax");
     });
-
-    it('Valid login attempt', () => {
-        cy.login()
-        cy.get('.nav-tabs>li>a').should('contain', 'My Organizations')
-    })
-
-})
+  });
+});
