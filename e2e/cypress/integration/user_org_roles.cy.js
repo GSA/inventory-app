c86e6eedd9f8d1d721a904b076daaa4f54a2e5bd
Fuhu Xia <fxia@reisystems.com>
2026-09-17 10:34:42 -0400

    re-position deletion icon

--- e2e/cypress/integration/user_org_roles.cy.js
+++ e2e/cypress/integration/user_org_roles.cy.js
@@ -127,6 +127,7 @@ describe('User organization roles', () => {
                     cy.wrap(userRows[0])
                         .find('a.user-org-roles-delete')
                         .should('have.attr', 'aria-label', 'Delete user')
+                        .and('have.css', 'float', 'right')
                         .find('i')
                         .should('have.class', 'fa-user-xmark');
                     const memberships = userRows[0].querySelectorAll(
